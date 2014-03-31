5fc95b6e70bd0b180e8d0bb0076c128b8e775d65
digityler <tysclark@gmail.com>
2014-03-31 15:19:30 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xhire::Application.config.secret_key_base = 'dfa0716611e084fe43fa25271e27ca138b707303db1ecf0373be9aeac91165582cedc50145cd9f8f878b6235a3971519e4cce5b390b1f54f185734c3e0c76a7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xhire::Application.config.session_store :cookie_store, key: '_xhire_session'
