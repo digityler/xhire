82d0b8462b68835d2a608ea0a45182cc0825d5da
digityler <tysclark@gmail.com>
2014-04-07 18:20:27 -0400

    static pages correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Xhire::Application.routes.draw do
   root 'static#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help',    to: 'static#help',    via: 'get'
+  match '/about',   to: 'static#about',   via: 'get'
+  match '/contact', to: 'static#contact', via: 'get'
   match '/signup',   to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
